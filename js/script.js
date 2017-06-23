dbc11be581af6aba18e474a56d7d18141c470360
Inspector Rachko <tris@berlin.com>
2017-06-23 21:28:02 +0200

    Beta 1.0

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+var link = document.querySelector(".main-nav__toggle");
+var menu = document.querySelector(".main-nav");
+var html = document.documentElement;
 
- var link = document.querySelector(".main-nav__toggle"); 
- var menu = document.querySelector(".main-nav")
 
-link.addEventListener("click", function(event) {
- event.preventDefault();
- menu.classList.toggle("main-nav--closed");
- }); 
+var form = document.querySelector(".contest-form__submit");
+var success = document.querySelector(".modal-success");
+var error = document.querySelector(".modal-error");
+var name = document.querySelector("[name=name]");
+var surname = document.querySelector("[name=surname]");
+
+
+html.className = html.className.replace("no-js", "js");
+
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    menu.classList.toggle("main-nav--opened");
+});
